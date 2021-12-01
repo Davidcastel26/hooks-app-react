cd946ec21f4717b1849a1742eb828a0ba52db7c3
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-01 13:15:42 -0600

    adding state counter app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HookApp from './HookApp';
+import CounterApp from './components/useState/CounterApp';
+// import HookApp from './HookApp';
 
 
 ReactDOM.render(
   
-    <HookApp />,
+    // <HookApp />,
+    <CounterApp />,
   document.getElementById('root')
 );
 
